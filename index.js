95bd92d1e19b40f1738c0e2c8073c67fae9520ad
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-18 08:53:21 -0700

    not near done yet

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs').promises;
 
-const readFilePaths = ['./package.json'];
+const readFilePaths = './package.json';
   
-const promisesForEachFile = readFilePaths.map(path => fs.readFile(path, { encoding: 'utf8' }));
+const promisesForEachFile = fs.readFile(readFilePaths, { encoding: 'utf8' });
 Promise.allSettled(promisesForEachFile)
   .then(file => {
     console.log(file);
@@ -13,6 +13,14 @@ Promise.allSettled(promisesForEachFile)
 
 fs.writeFile('./File.txt', 'im in a not').then(console.log('done'));
 
+Promise.allSettled(promisesForEachFile)
+  .then(file => {
+    fs.writeFile('./File.txt', file).then(console.log('done'));
+  });
+
+fs.readFile(readFilePaths, { encoding: 'utf8' }).then(read => console.log(read));
+
+//fs.readFile(readFilePaths, { encoding: 'utf8' }).then(fs.writeFile('./File.txt', 'im in a not').then(console.log('done')));
 
 
 
